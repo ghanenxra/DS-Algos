7efe6cdf219e8e534b63d0800da1751609849679
Ghanendra Chakradhari <pureghanendra@gmail.com>
2026-06-13 05:07:05 +0530

    Add files via upload

---
+++ LeetCode/74. Search a 2D matrix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows , cols = len(matrix), len(matrix[0])
+        left, right = 0 , (rows*cols)-1
+
+        while left<=right:
+            mid = (left+right)//2
+            mid_element = matrix[mid//cols][mid%cols]
+            if mid_element == target:
+                return True
+            elif mid_element < target:
+                left = mid + 1
+            else:
+                right = mid -1
+        return False
+            
+
