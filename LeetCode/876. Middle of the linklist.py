92abcfa024966ff9847ec797a126b53788360740
Ghanendra Chakradhari <pureghanendra@gmail.com>
2026-07-01 04:01:40 +0530

    Add files via upload

---
+++ LeetCode/876. Middle of the linklist.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+      
+        curr = head
+        length = 0
+        while curr:
+            length += 1
+            curr = curr.next
+             
+        curr = head
+        for i in range(length // 2):
+            curr = curr.next
+            
+        return curr
